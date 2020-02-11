d82de005cc9bcb0d74dae079abf67f6facd1e56d
gaoxin <gao_xin@suixingpay.com>
2020-02-11 14:44:22 +0800

    完成bottomTab

---
+++ lib/page/member_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MemberPage extends StatelessWidget {
+  const MemberPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('商城会员中心'),
+    );
+  }
+}
